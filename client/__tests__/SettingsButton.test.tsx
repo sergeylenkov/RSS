9eff60789d817b267c27f3ca14e42e62803498fc
Sergey Lenkov <serglazy@gmail.com>
2021-04-18 20:02:58 +0300

    SettingsButton test snapshot

--- client/__tests__/SettingsButton.test.tsx
+++ client/__tests__/SettingsButton.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import TestRenderer from 'react-test-renderer';
+import renderer  from 'react-test-renderer';
 import SettingsButton from '../src/components/settings/SettingsButton';
 
 test('SettingsButton contains correct text', () => {
-  const testRenderer  = TestRenderer.create(<SettingsButton isActive onClick={() => { return; }} />);
-
-  console.log(testRenderer.toJSON());
+  const button = renderer.create(<SettingsButton isActive onClick={() => { return; }} />);
+  expect(button).toMatchSnapshot();
 });
